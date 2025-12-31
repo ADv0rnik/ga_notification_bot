20f2e41bceb8b49b5419ac462da385f103cfdce6
Alex Dvornik <aadvornik@gmail.com>
2025-12-31 15:15:52 +0100

    Finished function 'send_message_to_telegram'

--- configs.py
+++ configs.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.resolve()
 ENV_PATH = Path.joinpath(BASE_DIR, ".env")
-print(ENV_PATH)
+BASE_URL = "https://api.telegram.org/"
--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 import os
+import requests
+import asyncio
 from dotenv import load_dotenv
-from configs import ENV_PATH
+from configs import ENV_PATH, BASE_URL
 from aiogram import Bot, Dispatcher
-
 
 
 load_dotenv(ENV_PATH)
 bot_token = os.getenv("BOT_TOKEN")
+chat_id = os.getenv("CHAT_ID")
+uri = f"bot{bot_token}/sendMessage"
+test_msg = "Hello!"
 
 bot = Bot(bot_token)
 
+
+def send_message_to_telegram(msg: str, parse_mode: str = "HTML") -> bool:
+    payload = {
+        "chat_id": chat_id,
+        "text": msg,
+        "parse_mode": parse_mode
+    }
+
+    try:
+        response = requests.post(BASE_URL+uri, data=payload)
+        if response.ok:
+            return True
+        return False
+    except requests.RequestException:
+        return False
+    except Exception:
+        return False
+
+
+if __name__ == "__main__":
+    if send_message_to_telegram(test_msg):
+        print("Success")
+    else:
+        print("Failed")
